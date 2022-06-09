de619cdb6be2702d3f086bb7ce3d1d6ba2a5af03
MaxYak8 <107186885+MaxYak8@users.noreply.github.com>
2022-06-09 14:54:16 +0300

    Add files via upload

---
+++ AzureExample.cs
@@ -0,0 +1,62 @@
+// startup.cs liveness and readiness probes
+
+services.AddHealthChecks()
+                .AddCheck<StartupHostedServiceHealthCheck>(
+                    "hosted_service_startup",
+                    failureStatus: HealthStatus.Degraded,
+                    tags: new[] { "ready" });
+
+            services.AddHealthChecks()
+                .AddCheck<HealthCheckController>(
+                    "Database availability check",
+                    failureStatus: HealthStatus.Degraded,
+                    tags: new[] { "healthz", "ready" });
+
+app.UseEndpoints(endpoints => {
+                endpoints.MapHealthChecks("/healthz", new HealthCheckOptions { Predicate = (check) => check.Tags.Contains("healthz") });
+                endpoints.MapHealthChecks("/ready", new HealthCheckOptions { Predicate = (check) => check.Tags.Contains("ready") });
+
+                endpoints.MapControllers();
+            });
+			
+			
+// StartupHostedServiceHealthCheck.cs
+
+public class StartupHostedServiceHealthCheck : IHealthCheck {
+        private volatile bool _startupTaskCompleted = false;
+
+        public string Name => "slow_dependency_check";
+
+        public bool StartupTaskCompleted {
+            get => _startupTaskCompleted;
+            set => _startupTaskCompleted = value;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken)) {
+            if(StartupTaskCompleted) {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("The startup task is finished."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("The startup task is still running."));
+        }
+    }
+
+
+// HealthCheckController
+public class HealthCheckController : IHealthCheck {
+        private readonly IMediator _mediator;
+        public HealthCheckController(IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default) {
+            var result = await _mediator.Send(new GetAvailabilityDatabaseQuery());
+
+            return result.IsAvailable
+                ? HealthCheckResult.Healthy()
+                : HealthCheckResult.Unhealthy("Cannot reach db");
+        }
